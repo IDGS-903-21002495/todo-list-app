ef2b8507ac5d6f5efbcddc294730521aa44fb811
IDGS-903-21002495 <21002495@alumnos.utleon.edu.mx>
2025-11-05 04:12:28 -0600

    Add validations to DTOs

--- todo-backend/src/tasks/dto/create-task.dto.ts
+++ todo-backend/src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,25 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsBoolean,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
   title: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(500)
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   isCompleted?: boolean;
 }
